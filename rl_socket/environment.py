395fa9733d25d60746509823929061004abd53cb
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2022-10-11 20:14:07 +0300

    Added general idea on pipeline

--- rl_socket/environment.py
+++ rl_socket/environment.py
@@ -29,3 +29,28 @@ class Environment:
             np.power(
                 self.get_observation() - self.agent.get_target_points(), 2
                 ).sum(1)).sum()
+
+
+## from our favourite book, the general pipeline
+
+
+import gym
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+
+    total_reward = 0.0
+    total_steps = 0
+    obs = env.reset()
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+        if done:
+            break
+
+    print("Episode done in %d steps, total reward %.2f" % (
+        total_steps, total_reward))
